b87918988baa743bc19e39f5c843433ee3a04d01
in-seo <sis000512@gmail.com>
2022-04-07 01:10:38 +0900

    Project add, edit, delete logic

--- SouP/src/main/java/Matching/SouP/dto/project/ProjectForm.java
+++ SouP/src/main/java/Matching/SouP/dto/project/ProjectForm.java
@@ -13,7 +13,7 @@ public class ProjectForm {
     private Project_Method method;  // 프로젝트 or 스터디
     private String name;    // 프로젝트명
     private Project_Type project_Type;  //프로젝트분야
-    private MeetType meeting; // 대면 방식
+    private MeetType meet_Type; // 대면 방식
     private Project_Place place;   // 대면 지역
     private Project_Platform platform; //출시 플랫폼
     private String text; // 프로젝트설명
--- SouP/src/main/java/Matching/SouP/repository/ProjectInfoRepository.java
+++ SouP/src/main/java/Matching/SouP/repository/ProjectInfoRepository.java
@@ -2,8 +2,14 @@ package Matching.SouP.repository;
 
 import Matching.SouP.domain.project.ProjectInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectInfoRepository extends JpaRepository<ProjectInfo,Long> {
+
+    @Query("select p from ProjectInfo p order by p.id DESC")
+    List<ProjectInfo> findAllDesc();
 }
