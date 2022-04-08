fdae3d10b4b1dfca4d7002a02db52fb00f07dd30
in-seo <sis000512@gmail.com>
2022-04-09 01:50:01 +0900

    프로필 인증 추가

--- SouP/src/main/java/Matching/SouP/repository/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-//package Matching.SouP.repository;
-
-//import Matching.SouP.domain.posts.Posts;
-//import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface PostsRepository extends JpaRepository<Posts,Long> {
-//}
