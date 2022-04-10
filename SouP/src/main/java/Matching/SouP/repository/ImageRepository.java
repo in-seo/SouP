60f124f21e8553bbdacef55e7a069a5c6b8bf22f
in-seo <sis000512@gmail.com>
2022-04-10 12:47:00 +0900

    add Kakao Login

--- SouP/src/main/java/Matching/SouP/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-//package StudyMatching.cloneWeb.repository;
-
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ImageRepository extends JpaRepository<Project_image,Long> {
-//}
