340d09fe3b81d31ad87578210f71c52639c711bc
in-seo <sis000512@gmail.com>
2022-05-14 15:55:46 +0900

    add userid for detailForm

--- SouP/src/main/java/Matching/SouP/dto/project/DetailForm.java
+++ SouP/src/main/java/Matching/SouP/dto/project/DetailForm.java
@@ -1,19 +1,20 @@
 package Matching.SouP.dto.project;
 
 import Matching.SouP.domain.posts.Source;
+import lombok.Getter;
 
+@Getter
 public class DetailForm extends ShowForm {
 
     private String type;
+    private Long userId;
 
-    public DetailForm(Long id, String postName, String content, String userName, String date, String link, String stack, int views, String talk, Source source, int fav) {
+    public DetailForm(Long id, String postName, String content, String userName, String date, String link, String stack, int views, String talk, Source source, int fav, Long userId) {
         super(id, postName, content, userName, date, link, stack, views, talk, source, fav);
+        this.userId = userId;
     }
+
     public void setType(String type) {
         this.type = type;
-    }
-
-    public String getType() {
-        return type;
     }
 }
