4a0363ab78c3cb1075c241ef201f4f5ff7cc7528
in-seo <sis000512@gmail.com>
2022-05-16 23:26:36 +0900

    add CI/CD arguments(4-4)

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/SouP/dep
+PROJECT_NAME=SouP
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl SouP | grep jar | awk '{print $1}')
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/SouP/application-oauth-properties,/home/ec2-user/SouP/application-real-db.properties,classpath:/application-real.properties \
+        -Dspring.profiles.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
