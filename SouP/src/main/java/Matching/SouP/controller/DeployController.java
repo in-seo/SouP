bf2106c4f6942846760f71c26b0570d90c793765
in-seo <sis000512@gmail.com>
2022-05-18 23:45:49 +0900

    add nginx service

---
+++ SouP/src/main/java/Matching/SouP/controller/DeployController.java
@@ -0,0 +1,28 @@
+package Matching.SouP.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class DeployController {
+    private final Environment env;
+
+    @GetMapping("/vprofile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles()); // (1)
+        List<String> realProfiles = Arrays.asList("real","real1","real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+
+}
