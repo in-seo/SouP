19d3697483c1acc05792ebcf40ff292c2e71d5a6
in-seo <sis000512@gmail.com>
2022-05-19 13:34:48 +0900

    add Nginx Service(4-1)

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,7 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.
-properties,/home/ec2-user/SouP/application-oauth.properties,/home/ec2-user/SouP/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/SouP/application-oauth.properties,/home/ec2-user/SouP/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
