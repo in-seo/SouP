571cb99e73ee658c99583f4ebd08413491937f97
in-seo <sis000512@gmail.com>
2022-05-22 22:22:39 +0900

    add User Service

---
+++ SouP/src/main/java/Matching/SouP/service/UserService.java
@@ -0,0 +1,28 @@
+package Matching.SouP.service;
+
+import Matching.SouP.domain.posts.Post;
+import Matching.SouP.domain.project.ProjectConnect;
+import Matching.SouP.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    public List<Post> showPost(User user){
+        return user.getPostList();
+    }
+
+    public List<ProjectConnect> showFav(User user){
+        return user.getProjectConnectList();
+    }
+
+}
