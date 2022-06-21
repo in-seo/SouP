d9f5813ba9efb093ac33a768927b290d32c96687
in-seo <sis000512@gmail.com>
2022-06-21 23:30:58 +0900

    fix npm command error(5)

--- deploy/client/deploy.sh
+++ deploy/client/deploy.sh
@@ -6,12 +6,9 @@ PROJECT_NAME=SouP
 cd $REPOSITORY/zip
 
 . ~/.nvm/nvm.sh
-echo "> npx 버전"
-npx -v
+echo "> npm 버전"
+npm -v
 
-echo "> 3000번 포트 죽이기"
-lsof -i:3000
-kill -9 [PID]
 
 echo "> react 실행"
-npx next start
+npm start
