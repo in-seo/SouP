ffc1eccec7abaecb5e18deb8d9eba114b269a911
plrs9816 <61955474+plrs9816@users.noreply.github.com>
2022-06-22 16:04:05 +0900

    ci: deploy workflow

--- deploy/client/deploy.sh
+++ deploy/client/deploy.sh
@@ -3,12 +3,7 @@
 REPOSITORY=/home/ec2-user/SouP/dep/client
 PROJECT_NAME=SouP
 
-exit 0
 
-. ~/.nvm/nvm.sh
-echo "> npx 버전"
-npx -v
+echo "> deploy client"
 
-
-echo "> react 실행"
-nohup npx next start > $REPOSITORY/nohup-client.out 2>&1 &
+pm2 restart soup-client
