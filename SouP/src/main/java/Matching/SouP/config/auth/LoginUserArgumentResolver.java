3daa8d31de22346dac5290482742aca9b56530f0
in-seo <sis000512@gmail.com>
2022-07-05 15:50:54 +0900

    code optimize for local(2)

--- SouP/src/main/java/Matching/SouP/config/auth/LoginUserArgumentResolver.java
+++ SouP/src/main/java/Matching/SouP/config/auth/LoginUserArgumentResolver.java
@@ -18,9 +18,9 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        boolean isLoginUserAnnotiaion = parameter.getParameterAnnotation(LoginUser.class) !=null;
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) !=null;
         boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
-        return isLoginUserAnnotiaion && isUserClass;
+        return isLoginUserAnnotation && isUserClass;
     }
 
     @Override
