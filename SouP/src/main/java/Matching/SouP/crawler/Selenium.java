ae4bdc8b2b5fb148f9fec9731b85142d483ff64f
in-seo <sis000512@gmail.com>
2022-07-06 18:18:51 +0900

    options.addArguments("user-agent")

--- SouP/src/main/java/Matching/SouP/crawler/Selenium.java
+++ SouP/src/main/java/Matching/SouP/crawler/Selenium.java
@@ -20,7 +20,7 @@ public class Selenium {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
-        options.addArguments("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36");
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36");
         options.addArguments("lang=ko");
         options.setCapability("ignoreProtectedModeSettings", true);
         driver = new ChromeDriver(options);
