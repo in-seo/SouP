18fa7910ad68547662c097fa353e18a0f0cd70b0
in-seo <sis000512@gmail.com>
2022-08-23 00:19:55 +0900

    hotfix: optimize for okky

--- SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
+++ SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
@@ -2,6 +2,7 @@ package Matching.SouP.crawler.okky;
 
 import Matching.SouP.crawler.ConvertToPost;
 import Matching.SouP.crawler.CrawlerService;
+import Matching.SouP.crawler.Selenium;
 import Matching.SouP.domain.post.Source;
 import Matching.SouP.dto.project.ShowForm;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,54 +22,57 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class OkkyService extends CrawlerService{
-    private static final String urlOkky ="https://okky.kr/articles/gathering";  //https://okky.kr/articles/gathering?offset=24단위로
+    private static final String urlOkky ="https://okky.kr/events/gathering";
     private final OkkyRepository okkyRepository;
     private final ConvertToPost convertToPost;
 
-    public void getOkkyPostData() throws IOException{
+    public void getOkkyPostData() throws IOException, InterruptedException {
+        Selenium set = new Selenium();
+        WebDriver driver = set.getDriver();
+        driver.get(urlOkky);
         int start = recentPost();
         log.info("OKKY 크롤링 시작. {}번부터",start);
-        int Page = startPage(start);
-        while(Page>=0){
-            Document doc = Jsoup.connect(urlOkky + "?offset=" + 24 * Page).get();  // 페이지 선택.
-            for (int i = 24; i >0; i--) {  //오래된 글부터 크롤링  그럼 반드시 최신글은 DB에서 가장 밑에꺼임.
-                Elements element = doc.select("#list-article > div.panel.panel-default.gathering-panel > ul > li:nth-child(" + i + ")");
-                Elements title = element.select("div.list-title-wrapper.clearfix");
-                Elements information = element.select("div.list-group-item-author.clearfix");
-
-                String postName = title.select("h5 a").text();
-                    if(postName.contains("마감") || postName.contains("모집완료")) continue; //제목에 [마감]이 들어가있으면 마감
-                String num= title.get(0).select("span").text().substring(1);
+        int Page = startPage(driver,start);
+        while(Page>=1){
+            driver.get(urlOkky + "?page=" + Page);
+            String html = driver.getPageSource();
+            Document doc = Jsoup.parse(html);
+            for (int i = 21; i >0; i--) {  //오래된 글부터 크롤링  그럼 반드시 최신글은 DB에서 가장 밑에꺼임.
+                if(i==6)
+                    continue;
+                Elements element = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(5) > div > ul > li:nth-child(" + i + ")");
+                Elements title = element.select("div > div.my-2 > a");
+                String postName = title.text();
+//                if(postName.contains("마감") || postName.contains("모집완료")) continue; //제목에 [마감]이 들어가있으면 마감
+                String num= title.attr("href").substring(10);
                 if(Integer.parseInt(num)<=start){
                     Okky update = okkyRepository.findByNum(Integer.parseInt(num));
-                    if(update!=null){
-                        String temp = element.select("div.list-summary-wrapper.clearfix > div > ul > li:nth-child(3)").text();
-                        int views;
-                        if(temp.charAt(1)=='k')
-                            views = Integer.parseInt(temp.charAt(0)+"000");  //조회수가 1k 이상일경우
-                        else
-                            views = Integer.parseInt(temp);
-                        update.updateViews(views);
-                    }
+                    if(update!=null)
+                        update.updateViews(update.getViews()+10);
+
                     continue;   //이미 불러온 글이면 조회수만 업데이트 후 저장 X
                 }
-                String link = "https://okky.kr/article/"+num;
-                Document realPost = Jsoup.connect(link).get();
-                String content = realPost.select("#content-body > article").text();
-
+                String link = "https://okky.kr/articles/"+num;
+                Document realPost = click(driver, link);
+                String content = realPost.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div > div > div").text();
+                System.out.println("content = " + content);
                 StringBuilder stack = parseStack(postName,content);
-                String talk = realPost.select("#content-body > article").select("a").attr("href");
-                        if(talk.isEmpty()){ talk = parseTalk(content,talk);}
-
+                String talk = "";
+                talk = parseTalk(content,talk);
                 if(content.length()>200) {
                     content = content.substring(0, 199);
                 }
 
-                int views = Integer.parseInt(realPost.select("#article > div.panel.panel-default.clearfix.fa- > div.panel-heading.clearfix > div.content-identity.pull-right > div:nth-child(2)").text());
-
-                String userName = information.select("div > div > a").attr("title");
-                String date = information.select("div > div > div.date-created > span").text();
-                date = standard(date);
+                String userName = element.select("div > div:nth-child(1) > a:nth-child(2)").text();
+                String date = LocalDateTime.now().toString();
+                System.out.println(num);
+                System.out.println(postName);
+                System.out.println(content);
+                System.out.println(userName);
+                System.out.println(date);
+                System.out.println(link);
+                System.out.println(stack.toString());
+                System.out.println(talk);
                 Okky okky = new Okky(num,postName,content,userName,date,link,stack.toString(),talk);
                 okkyRepository.save(okky);
                 convertToPost.okky(okky);
@@ -75,21 +81,32 @@ public class OkkyService extends CrawlerService{
         }
     }
 
+    private Document click(WebDriver driver, String link) throws InterruptedException {
+        driver.navigate().to(link);
+        Thread.sleep(1000);
+        Document realPost = Jsoup.parse(driver.getPageSource());
+        driver.navigate().back();
+        return realPost;
+    }
+
     private String standard(String date) {
         date = date.substring(0,10)+"T"+ date.substring(11);
         date = LocalDateTime.parse(date).toString();
         return date;
     }
 
-    private int startPage(int start) throws IOException {
-        int page=1;  //page가 1이면 okky에선 2페이지이다..
+    private int startPage(WebDriver driver, int start) throws IOException {
+        int page=2;  //page가 1이면 okky에선 1페이지이다..
         /**
          * 디비에서 저장된 가장 최근 글이 1페이지에 있나 여부 판단. 만약 글 리젠이 많아서 2페이지 중반부터 크롤링 해야되면? 3페이지 첫글이 start보다 작아야 됌.
          * !!다음 페이지의 맨 첫 번째 글이, 가장 최근에 디비에 저장된 글의 번호보다 크면 다음 페이지로 넘어가야됌
          */
         while(true){
-            Document doc = Jsoup.connect(urlOkky + "?offset=" + 24 * page).get();
-            String sNum = doc.select("#list-article > div.panel.panel-default.gathering-panel > ul > li:nth-child(" + 1 + ") > div.list-title-wrapper.clearfix").get(0).select("span").text().substring(1);//각 페이지 첫 글
+            driver.get(urlOkky + "?page=" + page);
+            String html = driver.getPageSource();
+            Document doc = Jsoup.parse(html);
+            String sNum = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(5) > div > ul > li:nth-child(1) > div > div.my-2 > a").attr("href").substring(10);//각 페이지 첫 글
+            System.out.println(sNum+"번!");
             int num = Integer.parseInt(sNum);
             if(num<start){
                 log.info("{}페이지부터 시작",page-1);
@@ -111,7 +128,7 @@ public class OkkyService extends CrawlerService{
 
 //    @PostConstruct
 //    private void init() { //임시 기준점 -> 이 번호 이후의 글을 긁어온다.
-//        Okky temp = new Okky("1261666","임시 기준점","","","","","",64,"");
+//        Okky temp = new Okky("1301764","임시 기준점","","","","","","");
 //        okkyRepository.save(temp);
 //    }
 }
