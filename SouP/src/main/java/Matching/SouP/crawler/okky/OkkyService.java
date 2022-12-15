0dab7687f39dc42b4fceee78fea18c9cba96d6bc
in-seo <sis000512@gmail.com>
2022-12-15 20:11:58 +0900

    hotfix: update url(3)

--- SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
+++ SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
@@ -42,7 +42,7 @@ public class OkkyService extends CrawlerService{
                 for (int i = 21; i >0; i--) {  //오래된 글부터 크롤링  그럼 반드시 최신글은 DB에서 가장 밑에꺼임.
                     if(i==6)
                         continue;
-                    Elements element = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(6) > div > ul > li.py-4:nth-child(" + i + ")");
+                    Elements element = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(5) > div > ul > li.py-4:nth-child(" + i + ")");
                     Elements title = element.select("div > div.my-2 > a");
                     String postName = title.text();
                     String num="";
@@ -116,7 +116,8 @@ public class OkkyService extends CrawlerService{
             Document doc = Jsoup.parse(html);
             int num = 3000000;
             try {
-                String sNum = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(6) > div > ul > li:nth-child("+cnt+") > div > div.my-2 > a").attr("href").substring(10);//각 페이지 첫 글
+                String sNum = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(5) > div > ul > li:nth-child("+cnt+") > div > div.my-2 > a").attr("href").substring(10);//각 페이지 첫 글
+                #__next > main > div > div.w-full.min-w-0.flex-auto.lg\:static.lg\:max-h-full.lg\:overflow-visible > div > div.mb-9.sm\:mb-16 > div > ul > li:nth-child(1)
                 num = Integer.parseInt(sNum);
             }catch (StringIndexOutOfBoundsException e){
                 cnt++;
