c3ced73ba8abc15f5d9717f599b8b6503bec3194
inseo <sis000512@gmail.com>
2023-02-22 20:39:38 +0900

    fix: cp service RC(5)

--- SouP/src/main/java/Matching/SouP/crawler/CamPick/CampickService.java
+++ SouP/src/main/java/Matching/SouP/crawler/CamPick/CampickService.java
@@ -15,6 +15,7 @@ import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -109,14 +110,17 @@ public class CampickService extends CrawlerService {
         driver.get("https://www.campuspick.com/login");
         driver.findElement(By.cssSelector("#container > div.form > div > input:nth-child(1)")).sendKeys(PropertyUtil.getProperty("m.i"));
         driver.findElement(By.cssSelector("#container > div.form > div > input:nth-child(2)")).sendKeys(PropertyUtil.getProperty("m.p"));
+        WebElement loginButton = driver.findElement(By.cssSelector("#container > div.form > div > input.submit"));
         Thread.sleep(500);
         driver.switchTo().frame(0);
         log.warn("switch");
         driver.findElement(By.cssSelector("#recaptcha-anchor > div.recaptcha-checkbox-border")).click();
-        Thread.sleep(2000);
+        System.out.println("driver.findElement(By.cssSelector(\"#recaptcha-token\")).getAttribute(\"value\") = " + driver.findElement(By.cssSelector("#recaptcha-token")).getAttribute("value"));
         driver.switchTo().defaultContent();
+        Thread.sleep(2000);
         log.warn("backoff");
-        driver.findElement(By.cssSelector("#container > div.form > div > input.submit")).click();
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("arguments[0].click()", loginButton);
         Thread.sleep(1000);  //로그인 처리 대기
     }
     private String standard(String date) {
