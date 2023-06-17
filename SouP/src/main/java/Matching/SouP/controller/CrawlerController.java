6a01fd45e1f7c33c2254dd69222f8b0f9600f225
inseo <sis000512@gmail.com>
2023-06-17 17:45:48 +0900

    [서버 이전] deploy

--- SouP/src/main/java/Matching/SouP/controller/CrawlerController.java
+++ SouP/src/main/java/Matching/SouP/controller/CrawlerController.java
@@ -41,7 +41,7 @@ public class CrawlerController {
     private static LocalDateTime crawlTime;
 
     @CacheEvict(value = { "front", "featured" }, allEntries = true)
-    @Scheduled(fixedDelay = 3600000, initialDelay = 10000) //실행 후 20초 뒤에시작, 1시간마다 실행.
+//    @Scheduled(fixedDelay = 3600000, initialDelay = 10000) //실행 후 20초 뒤에시작, 1시간마다 실행.
     public void crawlList() throws InterruptedException, IOException {
         crawlTime = LocalDateTime.now();
         log.info("현 시각: {} , 크롤링 시작.", crawlTime);
