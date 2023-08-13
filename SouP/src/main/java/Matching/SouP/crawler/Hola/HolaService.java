42a962803eaa9e05a679bf2a8736d6b36250d20d
inseo <sis000512@gmail.com>
2023-08-13 18:56:08 +0900

    불필요한 의존관계 제거 및 가독성 향상(2)

--- SouP/src/main/java/Matching/SouP/crawler/Hola/HolaService.java
+++ SouP/src/main/java/Matching/SouP/crawler/Hola/HolaService.java
@@ -9,18 +9,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class HolaService extends CrawlerService {
-    private static final String urlHola = "https://holaworld.io";
+    private static String urlHola = "https://holaworld.io";
     private final HolaRepository holaRepository;
     private final ConvertToPost convertToPost;
 
@@ -32,26 +30,26 @@ public class HolaService extends CrawlerService {
         boolean flag = false;
         try {
             String standard = recentPost();
-            scroll((JavascriptExecutor) driver);  //전체스크롤
             String html = driver.getPageSource();
             Document doc = Jsoup.parse(html);
-            Elements element = doc.select("#root > main > div.mainContent_appWrapper___CgAh > ul");
-            log.info("훌라 크롤링 시작, 가장 최신글번호 = {}",standard);
+            Elements element = doc.select("#root > main > ul");
+            log.info("훌라 크롤링 시작, 가장 최신글번호 = {}", standard);
             int count = element.select(">a").size();
-            for (int i = count; i > 1; i--) {
+            log.warn("글 갯수 = {} ",count);
+            for (int i = count; i > 0; i--) {
                 if(i==count){
-                    scroll((JavascriptExecutor) driver);
-                    driver.findElement(By.cssSelector("#root > main > div.mainContent_appWrapper___CgAh > ul > a:nth-child(2)")).click();
+                    driver.findElement(By.cssSelector("#root > main > ul > a:nth-child(1)")).click();
                     String first = driver.getCurrentUrl().substring(27);
-                    if(first.compareTo(standard)<=0) {
+                    if(first.compareTo(standard) <= 0) {
                         log.warn("사이트 내 가장 최신글 번호 = {}, 따라서 불러올 글이 없습니다!",first);
                         return;
                     }
                     else
                         driver.navigate().back();
                 }
-                Elements eachPost = element.select("a:nth-child(" + i + ")");
-                driver.findElement(By.cssSelector("#root > main > div.mainContent_appWrapper___CgAh > ul > a:nth-child("+i+")")).click();
+                int aSelector = i*2-1; // 홀수번만 사용 예정
+                Elements eachPost = element.select("a:nth-child(" + aSelector + ")");
+                driver.get(urlHola + eachPost.attr("href"));
                 Thread.sleep(500);
                 Document realPost = Jsoup.parse(driver.getPageSource());
                 String link = driver.getCurrentUrl();
@@ -61,8 +59,8 @@ public class HolaService extends CrawlerService {
                     continue;   //이미 불러온 글이면 패스
                 }
                 driver.navigate().back();
-                String content = realPost.select("#root > div.studyContent_wrapper__VVyNH > div > div").text();
-                String talk = realPost.select("#root > div.studyContent_wrapper__VVyNH > div > div").select("a").attr("href");
+                String content = realPost.select("#root > div.studyContent_wrapper__VVyNH > div.studyContent_postContentWrapper__187Zh > div").text();
+                String talk = realPost.select("#root > div.studyContent_wrapper__VVyNH > div.studyContent_postContentWrapper__187Zh > div").select("a").attr("href");
                 if(talk.isEmpty()){talk = parseTalk(content,talk);}
                 if(talk.length()>200)
                     talk = talk.substring(0,199);
@@ -83,7 +81,7 @@ public class HolaService extends CrawlerService {
                 log.warn("불러올 글이 없습니다!");
             else
                 log.info("홀라 크롤링 성공");
-        } catch (StringIndexOutOfBoundsException | InterruptedException e) {
+        }catch (StringIndexOutOfBoundsException | InterruptedException e) {
             e.printStackTrace();
         } finally {
             driver.close(); // 브라우저 종료
@@ -93,17 +91,16 @@ public class HolaService extends CrawlerService {
     private String standard(String date) {
         date = date.substring(0,4)+'-'+date.substring(5,7)+'-'+date.substring(8,10)+'T'+ LocalDateTime.now().toLocalTime().toString().substring(0,8);
         date = LocalDateTime.parse(date).toString();
-
         return date;
     }
 
-    private void scroll(JavascriptExecutor driver) throws InterruptedException {
-        var stTime = new Date().getTime(); //현재시간
-        while (new Date().getTime() < stTime + 500) { //5초 동안 무한스크롤 지속
-            Thread.sleep(300); //리소스 초과 방지
-            driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        }
-    }
+//    private void scroll(JavascriptExecutor driver) throws InterruptedException {
+//        var stTime = new Date().getTime(); //현재시간
+//        while (new Date().getTime() < stTime + 500) { //5초 동안 무한스크롤 지속
+//            Thread.sleep(300); //리소스 초과 방지
+//            driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+//        }
+//    }
 
     public String recentPost(){
         return holaRepository.findRecent();
