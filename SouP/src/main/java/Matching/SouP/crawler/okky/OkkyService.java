f25e7f898ffb8f481c88bbaab08b74067e83f0e4
inseo <sis000512@gmail.com>
2023-11-23 16:51:45 +0900

    hotfix: okky 크롤링 구조 변환(3)

--- SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
+++ SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
@@ -35,7 +35,7 @@ public class OkkyService {
                 String html = driver.getPageSource();
                 Document doc = Jsoup.parse(html);
                 for (int i = 28; i > 0; i--) {  //오래된 글부터 크롤링  그럼 반드시 최신글은 DB에서 가장 밑에꺼임.
-                    Elements element = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(6) > div > ul > li.py-4:nth-child(" + i + ")");
+                    Elements element = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(5) > div > ul > li.py-4:nth-child(" + i + ")");
                     Elements title = element.select("div > div.my-2 > a");
                     String postName = title.text();
                     String num;
@@ -101,7 +101,7 @@ public class OkkyService {
             Document doc = Jsoup.parse(html);
             int num = Integer.MAX_VALUE;
             try {
-                String sNum = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(6) > div > ul > li:nth-child("+cnt+") > div > div.my-2 > a").attr("href").substring(10);//각 페이지 첫 글
+                String sNum = doc.select("#__next > main > div > div:nth-child(2) > div > div:nth-child(5) > div > ul > li:nth-child("+cnt+") > div > div.my-2 > a").attr("href").substring(10);//각 페이지 첫 글
                 num = Integer.parseInt(sNum);
             }catch (StringIndexOutOfBoundsException | NullPointerException e){
                 cnt++;
