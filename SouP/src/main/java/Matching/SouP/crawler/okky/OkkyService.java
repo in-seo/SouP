25b4860cf389e8bda0809fb2ef1fc51ae79c9ad4
in-seo <sis000512@gmail.com>
2025-11-01 18:58:15 +0900

    hotfix: okky(4)

--- SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
+++ SouP/src/main/java/Matching/SouP/crawler/okky/OkkyService.java
@@ -40,7 +40,7 @@ public class OkkyService {
                 Elements elements = doc.select("#__next > main > div > div:nth-child(2) > div:nth-child(3) > ul > li[class^=\"px\"]\n");
                 for (int i = elements.size() - 1; i >= 0; i--) {
                     Element element = elements.get(i);
-                    Elements title = element.select("div > div.my-2 > div > a");
+                    Elements title = element.select("div > div:nth-child(2) > div > a.my-2");
                     // 여기서 각 element에 대한 처리를 진행
                     String postName = title.text();
                     String num = "";
@@ -112,14 +112,15 @@ public class OkkyService {
             String html = driver.getPageSource();
             Document doc = Jsoup.parse(html);
             int num = Integer.MAX_VALUE;
+            String href = "";
             try {
                 Elements elements = doc.select("#__next > main > div > div:nth-child(2) > div:nth-child(3) > ul > li[class^=\"px\"]\n");
-                String href = elements.get(0).select("div > div.my-2 > div > a").attr("href"); // 각 페이지 첫 글의 번호를 통해 페이지를 선택하자.
+                href = elements.get(0).select("div > div:nth-child(2) > div > a.my-2").attr("href"); // 각 페이지 첫 글의 번호를 통해 페이지를 선택하자.
                 String sNum = href.substring(10, href.lastIndexOf('?'));
                 num = Integer.parseInt(sNum);
             }catch (StringIndexOutOfBoundsException | NullPointerException e){
                 cnt++;
-                log.info("StringIndexOutOfBoundsException");
+                log.info("[StringIndexOutOfBoundsException] : " + href);
                 SlackNotifier slackNotifier = new SlackNotifier();
                 slackNotifier.sendMessageToSlack(e.getMessage());
                 continue;
